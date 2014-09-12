ddba59f34f6f3234e722e0649126373930b43864
Haasio Lassi S <lhaasio@da2-ex-dk107-12.helsinki.fi>
2014-09-12 12:06:20 +0300

    välillä on hyvä tallentaa

--- htdocs/lib/kayttaja.php
+++ htdocs/lib/kayttaja.php
@@ -1,4 +1,7 @@
 <?php
+
+require_once 'lib/tietokantayhteys.php';
+
 class Kayttaja {
   
     private $id;
@@ -7,9 +10,25 @@ class Kayttaja {
 
     public function __construct($id, $kayttajatunnus, $salasana) {
         $this->id = $id;
-        $this->kayttajatunnus = $tunnus;
+        $this->kayttajatunnus = $kayttajatunnus;
         $this->salasana = $salasana;
     }
+
+    public static function get_kayttajat() {
+        $sql = 'SELECT id, kayttajatunnus, salasana from kayttajat';
+        $kysely = getTietokantayhteys()->prepare($sql); $kysely->execute();
+        
+        $tulokset = array();
+        foreach($kysely->fetchAll(PDO::FETCH_OBJ) as $tulos) {
+            $id = $tulos->id;
+            $kayttajatunnus = $tulos->kayttajatunnus;
+            $salasana = $tulos->salasana;
+            $kayttaja = new Kayttaja($id, $kayttajatunnus, $salasana);
+            $tulokset[] = $kayttaja;
+        }
+        return $tulokset;
+    }
+
 
     
 
@@ -23,5 +42,14 @@ class Kayttaja {
     }
     public function get_id(){
         return $this->id;
+    }
+    public function set_kayttajatunnus($tunnus){
+        $this->kayttajatunnus = $tunnus;
+    }
+    public function set_salasana($salasana){
+        $this->salasana = $salasana;
+    }
+    public function set_id($id){
+        $this->id = $id;
     }
 }
--- htdocs/tietokantatesti.php
+++ htdocs/tietokantatesti.php
@@ -1,12 +1,28 @@
 <?php
-require 'lib/tietokantayhteys.php';
+require_once 'lib/tietokantayhteys.php';
+require_once 'lib/kayttaja.php';
 
-$yhteys = getTietokantayhteys();
+$kayttajat = Kayttaja::get_kayttajat();?><!DOCTYPE html>
+<html>
+    <head>
+	    <link href="../css/bootstrap.css" rel="stylesheet">
+	    <link href="../css/bootstrap-theme.css" rel="stylesheet">
+	    <link href="../css/main.css" rel="stylesheet">
+        <title>Muistilista</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+        <h1>Käyttäjät</h1>
+        <table class="table">
+            <tr> <th>Käyttäjänimi</th><th>Salasana</th></tr>  
+            <?php foreach ($kayttajat as $kayttaja) {?>
+                <tr><td><?php $kayttaja->get_kayttajatunnus();?></td>
+                    <td><?php $kayttaja->get_salasana();?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
 
-$sql = 'SELECT id, kayttajatunnus, salasana from kayttajat';
-$kysely = $yhteys->prepare($sql);
-$kysely->execute();
 
-$assosiaatiotaulu = $kysely->fetch();
-echo $assosiaatiotaulu['kayttajatunnus'];
+
+
 
