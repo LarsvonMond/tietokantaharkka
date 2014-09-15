656e6ac7815aa404e6e28f0d2b9b2422340dd70a
Haasio Lassi S <lhaasio@da1-cs-b221-18.helsinki.fi>
2014-09-15 16:12:15 +0300

    Saatanan Gedit kaatu taas nyt meen himaan

--- views/login.php
+++ views/login.php
@@ -12,11 +12,12 @@
     </head>
     <body>
         <h1>Muistilista</h1>
-        <label for="kayttajatunnus">Käyttäjätunnus</label>
-        <input type="text" class="form-control" id="kayttajatunnus" placeholder="Käyttäjätunnus">
-        <label for="salasana">Salasana</label>
-        <input type="password" class="form-control" id="salasana" placeholder="Salasana">       
-        <a href="askarelistaus.html">Kirjaudu</a>
-
+        <form action="login.php" method="POST">
+            <label for="kayttajatunnus">Käyttäjätunnus</label>
+            <input type="text" class="form-control" name="kayttajatunnus" placeholder="Käyttäjätunnus">
+            <label for="salasana">Salasana</label>
+            <input type="password" class="form-control" name="salasana" placeholder="Salasana">       
+            <button class="btn btn-default" type="submit">Kirjaudu</button>
+        </form>
     </body>
 </html>
--- views/pohja.php
+++ views/pohja.php
@@ -8,15 +8,6 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     </head>
     <body>
-        <h1>Muistilista</h1>
-        <ul class="nav nav-pills" role="tablist">
-            <li class="active"><a href="#">Askarelista</a></li>
-            <li><a href="lisaa_askare.html">Lisää askare</a></li>
-            <li><a href="#">Vaihda salasana</a></li>
-            <li><a href="kirjaudu.html">Kirjaudu ulos</a></li>
-        </ul>
-        <div id="content">
         <?php require 'views/'.$sivu; ?>
-        </div>
     </body>
 </html>
