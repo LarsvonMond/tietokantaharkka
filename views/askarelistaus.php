13b455726be770472d1aba3428c88f1360eff7c3
Haasio Lassi S <lhaasio@da2-ex-dk107-11.helsinki.fi>
2014-09-18 15:08:22 +0300

    Askarelistaus toimii

--- views/askarelistaus.php
+++ views/askarelistaus.php
@@ -8,9 +8,9 @@
         <tr> <th>Askare</th><th>Luokat</th><th>Tärkeysaste</th><th></th></tr>  
         <?php foreach($data->askareet as $askare): ?>
             <tr> <td><?php echo $askare->get_kuvaus(); ?> </td> 
-            <td><?php foreach($askare->get_luokat() as $luokka): ?>
-                    <?php echo $luokka->get_nimi(); ?>
-                    <?php echo ', '; ?>
+            <td>
+                <?php foreach($askare->get_luokat() as $luokka): ?>
+                    <?php echo $luokka; ?>
                 <?php endforeach; ?></td>
             <td><?php echo $askare->get_tarkeys(); ?></td>
             <td><a href="#">Poista</a></td></tr>
